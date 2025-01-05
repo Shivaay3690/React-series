aed6a96c21795a8cefea47c8740717f4659f9a84
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2025-01-05 17:59:52 +0000

    tailwind injecting with react

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  
+
+  return (
+    <>
+       <h1 className='bg-green-200 text-black rounded-md mb-4'>Tailwind Test</h1>
+      <Card/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
